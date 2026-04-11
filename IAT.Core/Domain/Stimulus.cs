553fdedf50a6a4cff92c65e99681045e28021d81
Michael Janda <admin@iatsoftware.net>
2026-04-11 13:07:30 -0400

    Domain Model Objects

--- IAT.Core/Domain/Stimulus.cs
+++ IAT.Core/Domain/Stimulus.cs
@@ -3,19 +3,35 @@ using IAT.Core.Enumerations;
 
 namespace IAT.Core.Domain;
 
+/// <summary>
+/// Defines a base class for stimuli used in an IAT experiment, providing common 
+/// properties and abstract methods for validation and display preview.
+/// </summary>
 public abstract partial class Stimulus : ObservableObject
 {
+    /// <summary>
+    /// The guid the stimulus will be identifiedby
+    /// </summary>
     [ObservableProperty]
     private Guid _id = Guid.NewGuid();
 
+    /// <summary>
+    /// Gets or sets the display order for the item.
+    /// </summary>
     [ObservableProperty]
     private int _displayOrder;
 
+    /// <summary>
+    /// Gets or sets the originating block for the stimulus.
+    /// </summary>
     [ObservableProperty]
     private int _originatingBlock;
-
+    
+    /// <summary>
+    /// Gets or sets the keyed direction for the stimulus.
+    /// </summary>
     [ObservableProperty]
-    private KeyedDirection _keyedDirectionName = KeyedDirection.None;
+    private KeyedDirection _keyedDirection = KeyedDirection.None;
 
     // Pure domain behavior only
     public abstract ValidationResult Validate();
